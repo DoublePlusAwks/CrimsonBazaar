d881e44033e3c3a62e730a43a57a01d4563972aa
Spencer Kim <sabin1997@gmail.com>
2017-11-30 20:30:52 -0500

    Adds ItemForm

--- components/auctions/AuctionCard.js
+++ components/auctions/AuctionCard.js
@@ -3,27 +3,35 @@ import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import * as moment from 'moment';
 import * as pluralize from 'pluralize';
 
-const AuctionCard = ({ auctionId, auction }) => {
-  return (
-    <View style={styles.container}>
-      <TouchableOpacity style={{ flex: 1 }}>
-        <View>
-          <Text>
-            {auction.title}
-          </Text>
-        </View>
-        <View>
-          <Text>
-            {`Market closes ${moment.default(auction.end).fromNow()}`}
-          </Text>
-          <Text>
-            {`Contains ${pluralize.default('item', auction.items.length, true)}`}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
-};
+class AuctionCard extends Component {
+  render() {
+    const { auctionId, auction } = this.props;
+    const { navigate } = this.props.navigation;
+    console.log(this.props.navigation);
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          style={{ flex: 1 }}
+          onPress={() => navigate("ItemForm")}
+          >
+          <View>
+            <Text>
+              {auction.title}
+            </Text>
+          </View>
+          <View>
+            <Text>
+              {`Market closes ${moment.default(auction.end).fromNow()}`}
+            </Text>
+            <Text>
+              {`Contains ${pluralize.default('item', auction.items.length, true)}`}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
--- components/item-form/ItemForm.js
+++ components/item-form/ItemForm.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { TouchableOpacity, Image, View, Text } from 'react-native';
 import { ImagePicker } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
 
 class ItemForm extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      title: '',
+      description: '',
+      image: '',
+    };
+  }
+
+  _pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [1, 1],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
   }
 
   render() {
+    const { image } = this.state;
     return (
       <View>
-        <Text>
-          ItemForm
-        </Text>
+        <TouchableOpacity
+          onPress={() => this._pickImage()}
+        >
+          <View>
+            {image ?
+              <Image
+                source={{ uri: image }}
+                style={{ width: 200, height: 200 }}
+              />
+              :
+              <Ionicons name="md-camera" size={128} color="gray" />
+            }
+          </View>
+        </TouchableOpacity>
       </View>
     );
   }
